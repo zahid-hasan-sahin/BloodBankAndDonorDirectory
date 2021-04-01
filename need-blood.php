6c9245c927fda1769cf60a1d9d01a12371057a3b
SK-Rahat-Islam <81740329+SK-Rahat-Islam@users.noreply.github.com>
2021-04-01 16:46:50 +0600

    Update need-blood.php

--- need-blood.php
+++ need-blood.php
@@ -158,6 +158,26 @@
                         else if(diviList == 'Dhaka') {
                             var disctList = '<option disabled selected>Select District</option><option value="Dhaka">Dhaka</option><option value="Faridpur">Faridpur</option><option value="Gazipur">Gazipur</option><option value="Gopalganj">Gopalganj</option><option value="Kishoreganj">Kishoreganj</option><option value="Madaripur">Madaripur</option><option value="Manikganj">Manikganj</option><option value="Munshiganj">Munshiganj</option><option value="Narayanganj">Narayanganj</option><option value="Narsingdi">Narsingdi</option><option value="Rajbari">Rajbari</option><option value="Shariatpur">Shariatpur</option><option value="Tangail">Tangail</option>';
                         }
+                        // // set Khulna division districts
+                        else if(diviList == 'Khulna') {
+                            var disctList = '<option disabled selected>Select District</option><option value="Bagerhat">Bagerhat</option><option value="Chuadanga">Chuadanga</option><option value="Jessore">Jessore</option><option value="Jinaidaha">Jinaidaha</option><option value="Khulna">Khulna</option><option value="Magura">Magura</option><option value="Meherpur">Meherpur</option><option value="Narail">Narail</option><option value="Satkhira">Satkhira</option>';
+                        }
+                        // set Mymensingh division districts
+                        else if(diviList == 'Mymensingh') {
+                            var disctList = '<option disabled selected>Select District</option><option value="Mymensingh">Mymensingh</option><option value="Netrokona">Netrokona</option><option value="Jamalpur">Jamalpur</option><option value="Sherpur">Sherpur</option>';           
+                        }
+                        // set Rajshahi division districts
+                        else if(diviList == 'Rajshahi') {
+                            var disctList = '<option disabled selected>Select District</option><option value="Rajshahi">Rajshahi</option><option value="Natore">Natore</option><option value="Pabna">Pabna</option><option value="Bogura">Bogura</option><option value="Chapainawabganj">Chapainawabganj</option><option value="Joypurhat">Joypurhat</option><option value="Naogaon">Naogaon</option><option value="Sirajganj">Sirajganj</option>';
+                        }
+                        // set Rangpur division districts
+                        else if(diviList == 'Rangpur') {
+                            var disctList = '<option disabled selected>Select District</option><option value="Rangpur">Rangpur</option><option value="Dinajpur">Dinajpur</option><option value="Kurigram">Kurigram</option><option value="Nilphamari">Nilphamari</option><option value="Gaibandha">Gaibandha</option><option value="Thakurgaon">Thakurgaon</option><option value="Panchagarh">Panchagarh</option><option value="Lalmonirhat">Lalmonirhat</option>';
+                        }
+                        // set Sylhet division districts
+                        else if(diviList == 'Sylhet') {
+                            var disctList = '<option disabled selected>Select District</option><option value="Habiganj">Habiganj</option><option value="Moulvibazar">Moulvibazar</option><option value="Sunamganj">Sunamganj</option><option value="Sylhet">Sylhet</option>';           
+                        }
                         else if(diviList == 'Select Division') {
                             var disctList = '<option value="" disabled selected>Select District</option>';
                         }
